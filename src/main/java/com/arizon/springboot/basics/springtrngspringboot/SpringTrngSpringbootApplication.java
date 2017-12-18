c3eb01f230c1990e8fb884e0ad1097a4a4b17b01
Sundararaju, Gopinath <gopinath.sundararaju@pearson.com>
2017-12-18 15:40:29 -0600

    spring boot web application structure

---
+++ src/main/java/com/arizon/springboot/basics/springtrngspringboot/SpringTrngSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.arizon.springboot.basics.springtrngspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrngSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTrngSpringbootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/arizon/springboot/basics/springtrngspringboot/SpringTrngSpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.arizon.springboot.basics.springtrngspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTrngSpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
